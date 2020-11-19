45e2d9056a0507fa4a9636314045e15a5eea637e
Немыкина Алина Николаевна <aalnemykina11400@gmail.com>
2020-11-19 12:36:38 +0300

    Tasks.vue modification

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card mb-3" style="max-width: 540px;">
+    <a href="#">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img :src="task.imagePath" class="card-img" alt="..." />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Задание {{task.id}}. {{task.category}}</h5>
+            <p class="card-text">{{task.title}}</p>
+            <p class="card-text">
+              <small class="text-muted"
+              >{{task.explanation}}</small
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: ['task']
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -1,96 +1,56 @@
 <template>
   <div>
     <div class="container py-5">
-      <h1 class="jumbotron-heading">План дня</h1>
+      <h3 class="jumbotron-heading">План дня</h3>
     </div>
     <div class="album">
-      <div class="container">
-        <div class="card mb-3" style="max-width: 540px;">
-          <a href="#">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="" class="card-img" alt="..." />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Задание 1. Прокачаем грамматику?</h5>
-                  <p class="card-text">Present Simple</p>
-                  <p class="card-text">
-                    <small class="text-muted"
-                      >Много грамматики не бывает!</small
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="card mb-3" style="max-width: 540px;">
-          <a href="#">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="" class="card-img" alt="..." />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">
-                    Задание 2. Слабо отгадать все слова?
-                  </h5>
-                  <p class="card-text">Перевод-слово</p>
-                  <p class="card-text">
-                    <small class="text-muted">Давай попробуем вместе!</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="card mb-3" style="max-width: 540px;">
-          <a href="#">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="" class="card-img" alt="..." />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Задание 3: Читай скорее текст!</h5>
-                  <p class="card-text">Good friends are like stars.</p>
-                  <p class="card-text">
-                    <small class="text-muted">И добавляй слова</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="card mb-3" style="max-width: 540px;">
-          <a href="#">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="img/translation.jpeg" class="card-img" alt="..." />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Задание 4. Смотри классное видео!</h5>
-                  <p class="card-text">
-                    Clothes Vocabulary 1 - Sentences by ELF Learning
-                  </p>
-                  <p class="card-text">
-                    <small class="text-muted">И учи незнакомые слова!</small>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+      <div class="container" v-for="item in items" v-bind:key="item">
+        <Task v-bind:task="item"></Task>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Task from "@/components/Task";
+
 export default {
-  name: "Tasks"
+  name: "Tasks",
+  components: {Task},
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          category: "Прокачаем грамматику?",
+          title: "Present Simple",
+          explanation: "Много грамматики не бывает!",
+          imagePath: "img/grammar.png"
+        },
+        {
+          id: 2,
+          category: "Слабо отгадать все слова?",
+          title: "Перевод-слово",
+          explanation: "Давай попробуем вместе!",
+          imagePath: "img/translation_.jpeg"
+        },
+        {
+          id: 3,
+          category: "Читай скорее текст!",
+          title: "Good friends are like stars.",
+          explanation: "И добавляй слова",
+          imagePath: "img/reading.jpeg"
+        },
+        {
+          id: 4,
+          category: "Смотри классное видео!",
+          title: "Clothes Vocabulary 1 - Sentences by ELF Learning",
+          explanation: "И учи незнакомые слова!",
+          imagePath: "img/translation.jpeg"
+        }
+      ]
+    }
+  }
 };
 </script>
 
